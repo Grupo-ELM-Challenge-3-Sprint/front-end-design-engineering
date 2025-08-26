be679ef80834d5c605bef2a34b6cfbedd63e740d
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-08-26 09:29:11 -0300

    Criando cabeçalho no react e criando projeto

---
+++ sprint-3/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,33 @@
+
+export default function Cabecalho() {
+  return (
+    <header>
+        <div className="container">
+            <div>
+                <a href="#" className="logo">
+                    <img src="./assets/img/imagem-index/imagem-logo.jpeg" alt="Logo SimplesHC" />
+                    <span>SimplesHC</span>
+                </a>   
+            </div>
+            <button className="menu-toggle" type="button" aria-label="Abrir menu" aria-expanded="false" aria-controls="primary-navigation">
+                <img src="./assets/img/icons/icone-botao.png" alt="Menu" className="menu-icon-img" />
+            </button>
+            <nav id="primary-navigation" aria-hidden="true"
+                    data-guide-step="3"
+                    data-guide-title="Navegação da página"
+                    data-guide-text="Use o menu acima para acessar páginas como Início, Serviços, FAQ e mais."
+                    data-guide-arrow="up">
+                <ul>
+                    <li><a href="#" className="active">Início</a></li>
+                    <li><a href="./assets/paginas/hospitais.html">Hospitais</a></li>
+                    <li><a href="./assets/paginas/servicos.html">Serviços</a></li>
+                    <li><a href="./assets/paginas/integrantes.html">Integrantes</a></li>
+                    <li><a href="./assets/paginas/faq.html">FAQ</a></li>
+                    <li><a href="./assets/paginas/contato.html">Contato</a></li>
+                    <li className="nav-item-login"><a href="./assets/paginas/area-paciente/meu-perfil.html" class="btn-nav-login">Entrar</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+  );
+}
---
+++ sprint-3/src/routes/EditarProdutos/index.tsx
@@ -0,0 +1,7 @@
+export default function EditarProdutos(){
+    return(
+        <main>
+            <h1>Editar os produtos</h1>
+        </main>
+    );
+}

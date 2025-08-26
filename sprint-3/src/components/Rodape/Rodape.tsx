2673175b9b89d1b6de4a8089288dfe30a8f8f11d
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-08-26 09:50:36 -0300

    Tentando fazer o tailwind funcionar. Não deu certo.

--- sprint-3/src/components/Rodape/Rodape.tsx
+++ sprint-3/src/components/Rodape/Rodape.tsx
@@ -1,7 +1,49 @@
 export default function Rodape() {
   return (
     <footer>
-      <p>&copy; Todos os direitos reservados - 2025. Meu App</p>
+        <div className="container footer-content">
+            <div className="footer-column about">
+                <h3>SimplesHC</h3>
+                <p>Simplificando o acesso à saúde para todos.</p>
+                <div className="social-icons">
+                    <a href="https://www.youtube.com/user/RedeLucyMontoro" title="Youtube da Rede Lucy Montoro" target="_blank" rel="noopener"><img src="../img/icons/icone-youtube.png" alt="Ícone Youtube"> </a> 
+                    <a href="https://www.instagram.com/redelucymontoro/" title="Instagram da Rede Lucy Montoro" target="_blank" rel="noopener"><img src="../img/icons/icone-instagram.png" alt="Ícone Instagram"> </a>
+                    <a href="https://twitter.com/redelucymontoro" title="Twitter da Rede Lucy Montoro" target="_blank" rel="noopener"><img src="../img/icons/icone-twitter.png" alt="Ícone Twitter"> </a>
+                </div>
+            </div>
+            <div className="footer-column links">
+                <h4>Navegação</h4>
+                <ul>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="./assets/paginas/hospitais.html">Hospitais</a></li>
+                    <li><a href="./assets/paginas/servicos.html">Serviços</a></li>
+                    <li><a href="./assets/paginas/integrantes.html">Integrantes</a></li>
+                    <li><a href="./assets/paginas/faq.html">FAQ</a></li>
+                    <li><a href="./assets/paginas/contato.html">Contato</a></li>
+                </ul>
+            </div>
+            <div className="footer-column links">
+                <h4>Serviços</h4>
+                <ul>
+                    <li><a href="./area-paciente/meus-exames.html">Resultados de Exames</a></li>
+                    <li><a href="./area-paciente/minhas-receitas.html">Receitas Médicas</a></li>
+                    <li><a href="./area-paciente/minhas-consultas.html">Agendamento</a></li>
+                    <li><a href="./area-paciente/minhas-consultas.html">Teleconsulta</a></li>
+                </ul>
+            </div>
+            <div className="footer-column contact">
+                <h4>Contato</h4>
+                <p>HC Hospital das Clínicas</p>
+                <p>Rua Dr. Ovídio Pires de Campos, 225</p>
+                <p>São Paulo - SP</p>
+                <p>CEP 05403-010</p>
+                <p>Telefone: (11) 2661-0000</p>
+                <p>Email: contato@simplify-hc.com.br</p>
+            </div>
+        </div>
+        <div className="footer-bottom">
+            <p>© 2025 SimplesHC. Todos os direitos reservados.</p>
+        </div>
     </footer>
   );
 }

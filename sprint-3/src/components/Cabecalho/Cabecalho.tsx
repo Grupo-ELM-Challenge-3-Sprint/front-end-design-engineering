9a895b16d3b993c08f525a76716468a713a2cae9
LuzBGouveia <lu.barros.gouveia2@gmail.com>
2025-08-26 10:31:04 -0300

    Criando as rotas para os componentes principais.

--- sprint-3/src/components/Cabecalho/Cabecalho.tsx
+++ sprint-3/src/components/Cabecalho/Cabecalho.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 export default function Cabecalho() {
   return (
@@ -17,15 +18,14 @@ export default function Cabecalho() {
                     data-guide-title="Navegação da página"
                     data-guide-text="Use o menu acima para acessar páginas como Início, Serviços, FAQ e mais."
                     data-guide-arrow="up">
-                <ul>
-                    <li><a href="#" className="active">Início</a></li>
-                    <li><a href="./assets/paginas/hospitais.html">Hospitais</a></li>
-                    <li><a href="./assets/paginas/servicos.html">Serviços</a></li>
-                    <li><a href="./assets/paginas/integrantes.html">Integrantes</a></li>
-                    <li><a href="./assets/paginas/faq.html">FAQ</a></li>
-                    <li><a href="./assets/paginas/contato.html">Contato</a></li>
-                    <li className="nav-item-login"><a href="./assets/paginas/area-paciente/meu-perfil.html" class="btn-nav-login">Entrar</a></li>
-                </ul>
+
+                    <Link to="/">Início</Link>
+                    <Link to="/hospitais">Hospitais</Link>
+                    <Link to="/servicos">Serviços</Link>
+                    <Link to="/integrantes">Integrantes</Link>
+                    <Link to="/faq">FAQ</Link>
+                    <Link to="/contato">Contato</Link>
+                    <Link to="/entrar">Entrar</Link>
             </nav>
         </div>
     </header>
--- sprint-3/src/main.tsx
+++ sprint-3/src/main.tsx
@@ -4,15 +4,23 @@ import App from './App.tsx';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './routes/Home/index.tsx';
-import Produtos from './routes/Produtos/index.tsx';
-import EditarProdutos from './routes/EditarProdutos/index.tsx';
 import Error from './routes/Error/index.tsx';
+import Servicos from './routes/Servicos/index.tsx';
+import Faq from './routes/Faq/index.tsx';
+import Contato from './routes/Contato/index.tsx';
+import Integrantes from './routes/Integrantes/index.tsx';
+import Hospitais from './routes/Hospitais/index.tsx';
+import Entrar from './routes/Entrar/index.tsx';
 
 const router = createBrowserRouter([
   {path:"/", element:<App/>, errorElement:<Error/>, children:[
     {path:"/", element: <Home/>},
-    {path:"/produtos", element: <Produtos/>},
-    {path:"/editar/produtos/:id", element: <EditarProdutos/>},
+    {path:"/servicos", element: <Servicos/>},
+    {path:"/hospitais", element: <Hospitais/>},
+    {path:"/integrantes", element: <Integrantes/>},
+    {path:"/faq", element: <Faq/>},
+    {path:"/contato", element: <Contato/>},
+    {path:"/entrar", element: <Entrar/>},
   ]}
 ]);
 

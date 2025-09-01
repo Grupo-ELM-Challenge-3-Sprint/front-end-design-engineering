ce44f0feacfcd1d2eaa508c1628d423eccfacbe9
LuzBGouveia <lu.barros.gouveia2@gmail.com>
2025-09-01 10:18:55 -0300

    Desfazendo o commit com erro

--- sprint-3/src/App.tsx
+++ sprint-3/src/App.tsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Rodape from "./components/Rodape/Rodape";
-import "./App.css"
 
 export default function App() {
 
   return (
-    <div>
+    <div className="containter">
       <Cabecalho/>
         <Outlet/>
       <Rodape/>

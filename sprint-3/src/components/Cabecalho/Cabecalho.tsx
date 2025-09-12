550275a69b940c312ecb68403595bb9e52d79bb5
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-09-12 08:08:18 -0300

    Arrumando o header

--- sprint-3/src/components/Cabecalho/Cabecalho.tsx
+++ sprint-3/src/components/Cabecalho/Cabecalho.tsx
@@ -7,8 +7,8 @@ export default function Cabecalho() {
     const linkClasses = ({ isActive }: { isActive: boolean }) => `block text-base p-4 ${isActive ? "text-blue-600 font-bold border-b-2 border-blue-600" : "text-gray-700 hover:text-blue-600"}`;
   return (
     <header className="bg-white py-2.5 border-b border-[#eee] relative top-0 z-[1000]" >
-        <div className="w-[90%] flex justify-between items-center relative">
-                <Link to="/" className="logo font-bold text-[#005c99] no-underline ml-12 z-[1001] flex items-center">
+        <div className="w-[90%] flex justify-between items-center relative m-5">
+                <Link to="/" className="logo font-bold text-[#005c99] no-underline z-[1001] flex items-center">
                     <img src={imagemLogo} alt="Logo SimplesHC" className="w-24 h-auto mr-2" />
                     <span className="text-lg text-[#005c99]">SimplesHC</span>
                 </Link>
